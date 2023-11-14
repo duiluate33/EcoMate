ef3cfc9efde643ef17b4e8bc46748971f3b4e898
duiluate33 <duiluate@naver.com>
2023-11-14 18:25:05 +0900

    메인화면 구성 : 버튼 제작 중 (에러O)

--- app/src/main/java/kr/ac/yeonsung/seoj/ecomate/MainActivity.java
+++ app/src/main/java/kr/ac/yeonsung/seoj/ecomate/MainActivity.java
@@ -1,29 +1,50 @@
 package kr.ac.yeonsung.seoj.ecomate;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
+import androidx.core.content.FileProvider;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.Toast;
-import android.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-import com.google.android.material.internal.BaselineLayout;
-import com.google.android.material.navigation.NavigationView;
+import androidx.annotation.Nullable;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout.DrawerListener ActionBarDrawerToggle;
+    ImageView imageView;
+    File file;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        File sdcard = Environment.getExternalStorageDirectory();
+        file = new File(sdcard, "capture.jpg");
+
+        imageView = findViewById(R.id.imageView);
+
+        Button btn_camera = findViewById(R.id.btn_camera);
+        btn_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                capture();
+            }
+        });
 
 
     }
@@ -45,6 +66,26 @@ public class MainActivity extends AppCompatActivity {
                 return true;
         }
         return false;
+    }
+
+
+
+    public void capture(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        Uri photoUri = FileProvider.getUriForFile(this, "com.example.myproject.fileprovider", file);
+        startActivityForResult(intent, 101);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 101  && resultCode == Activity.RESULT_OK){
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inSampleSize = 8;
+            Bitmap bitmap = BitmapFactory.decodeFile(file.getAbsolutePath(), options);
+            imageView.setImageBitmap(bitmap);
+        }
     }
 
 
