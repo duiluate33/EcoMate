c01ea9919ece52a36793f298aae01d367f797481
duiluate33 <duiluate@naver.com>
2023-11-28 12:22:07 +0900

    화면간연경 애러o

--- app/src/main/java/kr/ac/yeonsung/seoj/ecomate/MainActivity.java
+++ app/src/main/java/kr/ac/yeonsung/seoj/ecomate/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.core.content.FileProvider;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -28,6 +29,8 @@ import androidx.annotation.Nullable;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
@@ -38,11 +41,18 @@ public class MainActivity extends AppCompatActivity {
     Bitmap bitmap;
     Button btncamera, btngallery;
 
+    String mCurrentPhotoPath;
+    static final int REQUEST_TAKE_PHOTO = 1 ;
+
+    public static Context mContext;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         //File sdcard = Environment.getExternalStorageDirectory();
         file = new File(Environment.getExternalStorageDirectory().getAbsolutePath());
 
@@ -70,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        mContext = this;
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -92,14 +102,6 @@ public class MainActivity extends AppCompatActivity {
         return false;
     }
 
-
-
-    /*public void capture(){
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        Uri photoUri = FileProvider.getUriForFile(this, "kr.ac.yeonsung.seoj.fileProvider", file);
-        startActivityForResult(intent, 12);
-        //FileProvider.getUriForFile(this, "kr.ac.yeonsung.seoj.fileProvider",file);
-    }*/
     void getPermission() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if(checkSelfPermission(android.Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
@@ -143,5 +145,45 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
     }
 
-}
+    //카메라로 촬영한 이미지를 파일로 저장해주는 함수 생성
+    private File createImageFile() throws IOException{
+        //create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File StorageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                StorageDir
+        );
+        //save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
 
+    //카메라 인텐트를 실행하는 부분을 별도 함수로 생성
+    public void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        //ensure that's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            //찍은 이미지 파일을 어디로 보낼지 생성
+            File photofile = null;
+            try{
+                File photoFile = createImageFile();
+            }
+            catch (IOException ex) {
+                // create file했을 때 발생하는 에러
+            }
+
+            //파일이 성공적으로 생성됐을 때 continue
+            if (photofile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this, "kr.ac.yeonsung.seoj.fileprovider", photofile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent,REQUEST_TAKE_PHOTO);
+            }
+
+        }
+    }
+
+}
--- app/src/main/java/kr/ac/yeonsung/seoj/ecomate/ResultActivity.java
+++ app/src/main/java/kr/ac/yeonsung/seoj/ecomate/ResultActivity.java
@@ -2,18 +2,93 @@ package kr.ac.yeonsung.seoj.ecomate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.widget.Toolbar;
 
-public class ResultActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ResultActivity extends MainActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_result);
+
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar); //툴바를 액션바로 설정
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); //뒤로가기 버튼 활성화
+
+        Button btn_result_main = findViewById(R.id.btn_result_gomain);
+        Button btn_result_selectimage = findViewById(R.id.btn_result_selectimage);
+        Button btn_result_camera = findViewById(R.id.btn_result_camera);
+
+        btn_result_main.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_result_selectimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, 10);
+            }
+        });
+
+        btn_result_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,0);
+            }
+        });
     }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        try{
+            switch (requestCode) {
+                case REQUEST_TAKE_PHOTO: {
+                    if (resultCode == RESULT_OK) {
+                        File file = new File(mCurrentPhotoPath);
+                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), Uri.fromFile(file));
+                        if (bitmap != null) {
+                            imageView.setImageBitmap(bitmap);
+                        }
+                    }
+                    break;
+                }
+            }
+        } catch (IOException error) {
+            error.printStackTrace();
+        }
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_result_camera:
+                ((MainActivity)MainActivity.mContext).dispatchTakePictureIntent();
+                break;
+        }
+    }
+
+
 }
